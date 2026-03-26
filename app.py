df0c43418b2ceb9b8e0e74340db16d128553e691
Choigun <122153202+choigun0809A@users.noreply.github.com>
2026-03-26 15:49:17 +0800

    create

---
+++ app.py
@@ -0,0 +1,136 @@
+from flask import Flask, render_template, request, session, redirect, jsonify
+import firebase, json, os
+
+app = Flask(__name__)
+env = os.environ['WEB_SECRETKEY_FAM_TREE']
+app.config['SECRET_KEY'] = env
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        json_data = request.get_json()
+        email = json_data.get('email')
+        password = json_data.get('password')
+        GmailListed = firebase.GmailListed(email)
+        if GmailListed:
+            if not firebase.Verified(email):
+                return jsonify({'success': False, 'message': 'Email already exists! Wait For Verification.'}), 401
+            return jsonify({'success': False, 'message': 'Email already exists! Please Login.'}), 200
+
+        uid, isVerified = firebase.signup(email, password)
+        
+        if isVerified:
+            return jsonify({'success': True, 'message': 'Signup sent! Please Login.'}), 200
+        else:
+            return jsonify({'success': False, 'message': 'Signup sent! Please wait for verification.'}), 200
+
+    return render_template('signup.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        json_data = request.get_json()
+        email = json_data.get('email')
+        password = json_data.get('password')
+        uid, isVerified = firebase.login(email, password)
+        
+        if uid and isVerified:
+            session['uid'] = uid
+            session["verified"] = True
+            return jsonify({'success': True, 'message': 'Login successful! Redirecting...'}), 200
+        elif firebase.UidInRequests(uid) and isVerified:
+            return jsonify({'success': False, 'message': 'Invalid email or password.'}), 401
+        elif firebase.UidInRequests(uid) and not isVerified:
+            return jsonify({'success': False, 'message': 'Please wait for verification.'}), 401
+        elif not firebase.UidInRequests(uid):
+            return jsonify({'success': False, 'message': 'Please signup first.'}), 401
+
+    return render_template('login.html')
+
+
+@app.route('/get_all_members')
+def get_all_members():
+    members = firebase.get_all_members()
+    return jsonify(members), 200
+
+@app.route('/get_all_requests')
+def get_all_requests():
+    if not session.get('admin'):
+        return jsonify({'success': False, 'message': 'Unauthorized access!'}), 401
+    requests = firebase.get_all_requests()
+    return jsonify(requests), 200
+
+@app.route('/update_request', methods=['POST', 'GET'])
+def update_request():
+    if request.method == 'POST':
+        if not session.get('admin'):
+            return jsonify({'success': False, 'message': 'Unauthorized access!'}), 401
+        json_data = request.get_json()
+        uid = json_data.get('uid')
+        data = json_data.get('data')
+
+        firebase.update_request(uid, data)
+
+        return jsonify({'success': True, 'message': 'Request updated successfully!'}), 200
+
+@app.route('/update_member', methods=['POST'])
+def update_member():
+    firebase.update_member(request.json['key'], request.json['member'])
+    return jsonify({'success': True, 'message': 'Member updated successfully!'}), 200
+
+@app.route('/add_member', methods=['POST'])
+def add_member():
+    uniqueId = 0
+    while True:
+        if not firebase.check_member(request.json['name'], uniqueId):
+            break
+        uniqueId += 1
+    
+    firebase.add_member(request.json['name'], uniqueId, request.json['gender'], request.json['birthDate'])
+    return jsonify({'success': True, 'message': 'Member added successfully!'}), 200
+
+@app.route('/adminCheck', methods=['POST']  )
+def admin_check():
+    if request.method == 'POST':
+        json_data = request.get_json()
+        password = json_data.get('password')
+
+        if password == app.config['SECRET_KEY']:
+            session['admin'] = True
+            return jsonify({'success': True, 'message': 'Login successful! Redirecting...', 'redirect': '/admin/dashboard'})
+        else:
+            session['admin'] = False
+            return jsonify({'success': False, 'message': 'Login failed!', 'redirect': '/signup'})
+
+@app.route('/admin/dashboard')
+def admin_dashboard():
+    if not session.get('admin') or session.get('admin') == None:
+        return redirect('/admin')
+    return render_template('adminDashboard.html')
+
+@app.route('/admin')
+def admin_login():
+    return render_template('adminLogin.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if 'uid' not in session:
+        return redirect('/signup')
+
+    if not session["verified"]:
+        return redirect('/login')
+    
+    if firebase.UidInRequests(session['uid']):
+        if not firebase.VerifiedUid(session['uid']):
+            return redirect('/login')
+        
+    else:
+        return redirect('/signup')
+    
+    
+    return render_template('main_update.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5001)
---
+++ firebase.py
@@ -0,0 +1,123 @@
+from firebase_admin import credentials, initialize_app, db, firestore, auth, GoogleAuthCredentials
+import os, json
+
+envVarDir = os.getenv('FamilyTreeCred')
+envVar = None
+with open(envVarDir, 'r') as f:
+    envVar = json.load(f)
+
+# print(envVar)
+
+cred = credentials.Certificate(envVar)
+initialize_app(cred)
+
+db = firestore.client()
+
+requestRef = db.collection('requests')
+members_ref = db.collection('members')
+
+from firebase_admin import auth
+
+def GmailListed(email):
+    try:
+        auth.get_user_by_email(email)
+        return True
+    except auth.UserNotFoundError:
+        return False
+    except Exception as e:
+        print(e)
+        return False
+    
+def Verified(gmail):
+    try:
+        user = auth.get_user_by_email(gmail)
+        return requestRef.document(user.uid).get().to_dict().get('verified', False)
+    except Exception as e:
+        print(e)
+        return False
+
+def VerifiedUid(uid):
+    try:
+        return requestRef.document(uid).get().to_dict().get('verified', False)
+    except Exception as e:
+        print(e)
+        return False
+    
+def UidInRequests(uid):
+    try:
+        return requestRef.document(uid).get().exists
+    except Exception as e:
+        print(e)
+        return False
+
+def signup(email, password):
+    try:
+        user = auth.create_user(
+            email=email,
+            password=password
+        )
+
+        if Verified(email):
+            return user.uid, True
+
+        requestRef.add({
+            'uid': user.uid,
+            'email': email,
+            'verified': False,
+            'password': password
+        },
+        user.uid
+        )
+
+        return user.uid, False
+    
+    except Exception as e:
+        print(e)
+        return None, False
+
+def login(email, password):
+    try:
+        user = auth.get_user_by_email(email)
+        if Verified(email):
+            if db.collection('requests').document(user.uid).get().to_dict().get('password') == password:
+                return user.uid, True
+        
+        return user.uid, False
+    except Exception as e:
+        print(e)
+        return None
+
+def add_member(name, uniqueId = 0, gender = '', birthDate = ''):
+    members_ref.document(f"{name.lower()} ^ {uniqueId}").set({
+        'name': name,
+        'gender': gender,
+        'birthDate': birthDate,
+        'uniqueId': uniqueId,
+        'children': [],
+        'parents': []
+    })
+
+def update_member(key, member = {}):
+    members_ref.document(key).delete()
+    members_ref.document(f'{member.get("name").lower()} ^ {member.get("uniqueId")}').set(member)
+
+def check_member(name, uniqueId = 0):
+    return members_ref.document(f"{name} ^ {uniqueId}").get().exists
+
+def get_member(name, uniqueId = 0):
+    return members_ref.document(f"{name} ^ {uniqueId}").get().to_dict()
+
+def get_all_members():
+    members = {}
+    for member in members_ref.get():
+        members[member.id] = member.to_dict()
+    return members
+
+def get_all_requests():
+    requests = {}
+    for request in requestRef.get():
+        requests[request.id] = request.to_dict()
+    return requests
+
+def update_request(uid, data):
+    requestRef.document(uid).update(data)
